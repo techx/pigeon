df9f1cfbfca58d2d5166962f17a0e6be0d6d531b
andrew <andrewliu278@gmail.com>
2023-10-21 03:08:25 -0400

    redis

---
+++ server/embeddings.py
@@ -0,0 +1,230 @@
+import json
+import time
+
+import numpy as np
+import pandas as pd
+import redis
+import requests
+from redis.commands.search.field import (
+    NumericField,
+    TagField,
+    TextField,
+    VectorField,
+)
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+from redis.commands.search.query import Query
+from sentence_transformers import SentenceTransformer
+
+from textwrap import TextWrapper
+
+VECTOR_DIMENSION = 768
+
+# load redis client
+client = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+# load corpus
+with open('corpus.json', 'r') as f:
+    corpus = json.load(f)
+
+print(len(corpus))
+
+# load embedding model
+embedder = SentenceTransformer('msmarco-distilbert-base-v4')
+
+def load_corpus():
+    ''' loads corpus.json into redis
+    '''
+    print("loading corpus...")
+
+    pipeline = client.pipeline()
+    for i, doc in enumerate(corpus, start=1):
+        redis_key = f'documents:{i:03}'
+        pipeline.json().set(redis_key, "$", doc)
+    res = pipeline.execute()
+
+    if not all(res):
+        raise Exception('failed to load some documents')
+    print("successfully loaded all documents")
+
+
+def compute_embeddings():
+    ''' compute embeddings from redis documents
+    '''
+    print("computing embeddings...")
+
+    # get keys, questions, answers
+    keys = sorted(client.keys("documents:*"))
+    questions = client.json().mget(keys, "$.question")
+    answers = client.json().mget(keys, "$.answer")
+
+    assert(len(questions) == len(answers))
+
+    # compute embeddings
+    question_and_answer = [questions[i][0] + " " +
+                            answers[i][0] for i in range(len(questions))]
+    embeddings = embedder.encode(
+        question_and_answer).astype(np.float32).tolist()
+
+    # save embeddings
+    with open('embeddings.json', 'w') as f:
+        json.dump(embeddings, f)
+
+    VECTOR_DIMENSION = len(embeddings[0])
+
+    print("successfully computed embeddings")
+    return embeddings
+
+def load_embeddings(embeddings):
+    ''' load embeddings into redis
+    '''
+    print("loading embeddings into redis...")
+
+    # load embeddings into redis
+    pipeline = client.pipeline()
+    for i, embedding in enumerate(embeddings, start=1):
+        redis_key = f'documents:{i:03}'
+        pipeline.json().set(redis_key, "$.question_and_answer_embeddings", embedding)
+    res = pipeline.execute()
+    
+    if not all(res):
+        raise Exception('failed to load embeddings')
+    
+    print("successfully loaded all embeddings")
+
+def create_index():
+    ''' create search index in redis
+    assumes that documents and embeddings have already been loaded into redis
+    '''
+    print("creating index...")
+
+    schema = (
+        TextField("$.source", no_stem=True, as_name="source"),
+        TextField("$.question", no_stem=True, as_name="question"),
+        TextField("$.answer", no_stem=True, as_name="answer"),
+        VectorField(
+            "$.question_and_answer_embeddings",
+            "FLAT",
+            {
+                "TYPE": "FLOAT32",
+                "DIM": VECTOR_DIMENSION,
+                "DISTANCE_METRIC": "COSINE",
+            },
+            as_name="vector",
+        ),
+    )
+    definition = IndexDefinition(prefix=["documents:"], index_type=IndexType.JSON)
+    res = client.ft("idx:documents_vss").create_index(
+        fields=schema, definition=definition
+    )
+    
+    if (res == "OK"):
+        start = time.time()
+        while(1):
+            if str(client.ft("idx:documents_vss").info()['num_docs']) == str(len(corpus)):
+                info = client.ft("idx:documents_vss").info()
+                num_docs = info["num_docs"]
+                indexing_failures = info["hash_indexing_failures"]
+                print("num_docs", num_docs, "indexing_failures", indexing_failures)
+                return
+            if time.time() - start >= 60:
+                raise Exception('time out')
+    raise Exception('failed to create index')
+
+def create_query(k):
+    ''' create k-NN redis query
+    '''
+    return Query(f'(*)=>[KNN {k} @vector $query_vector AS vector_score]') \
+        .sort_by('vector_score') \
+        .return_fields('vector_score', 'source', 'question', 'answer') \
+        .dialect(2)
+
+def queries(query, queries):
+    ''' run queries against redis
+    :param query: redis query
+    :param queries: list of question queries
+    :return: list of dictionaries containing query and result
+    '''
+    print("running queries...")
+
+    # encode queries
+    encoded_queries = embedder.encode(queries)
+
+    # run queries
+    results_list = []
+    for i, encoded_query in enumerate(encoded_queries):
+        result_docs = (
+            client.ft("idx:documents_vss")
+            .search(
+                query,
+                {
+                    "query_vector": np.array(
+                        encoded_query, dtype=np.float32
+                    ).tobytes()
+                },
+            )
+            .docs
+        )
+        query_result = []
+        for doc in result_docs:
+            vector_score = round(1 - float(doc.vector_score), 2)
+            query_result.append(
+                {
+                    "score": vector_score,
+                    "source": doc.source,
+                    "question": doc.question,
+                    "answer": doc.answer,
+                }
+            )
+        results_list.append({"query": queries[i], "result": query_result})
+
+    return results_list
+
+def query_all(k, questions):
+    '''return k most similar documents for each query
+    '''
+    redis_query = create_query(k)
+    return queries(redis_query, questions)
+
+def embed_corpus():
+    ''' load corpus, compute embeddings, load embeddings into redis
+    '''
+    # flush database
+    client.flushdb()
+
+    # embed corpus
+    load_corpus()
+    embeddings = compute_embeddings()
+    load_embeddings(embeddings)
+    create_index()
+
+if __name__ == '__main__':
+    try:
+        embed_corpus()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        raise
+    
+    questions = ['What is the deadline to apply for the hackathon?',
+               'When is HackMIT?',
+               'What are the challenges?',
+               'How does judging work?',
+               'What building should I go to during the event?',
+               'What prizes are available?',
+               'How many people are allowed on a team?',
+               'What is HackMIT?',
+               'Can I attend HackMIT if I am an MIT grad student?',
+               'Can I attend HackMIT if I am a sophomore in high school?',
+               'I\'m a high school student, but I\'m really advanced. Can I attend HackMIT?',
+               'Do I need to bring money to the event?',
+               'Will we be able to sleep at the event?',
+               'Will we be able to stay overnight at the event?',
+               'What should I do if I am a beginner at the event?']
+    results = query_all(3, questions)
+    for result in results:
+        print(result['query'])
+        for doc in result['result']:
+            print(f"Score: {doc['score']}")
+            print(f"Source: {doc['source']}")
+            print(f"Q: {doc['question']}")
+            print(f"A: {doc['answer']}")
+        print()
