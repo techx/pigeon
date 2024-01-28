566b554497b14fd2f859c71079a770f1fc4812bb
andrew <andrewliu278@gmail.com>
2024-01-28 05:16:36 -0500

    modify access denied text and margin

--- client/src/routes/restricted.tsx
+++ client/src/routes/restricted.tsx
@@ -4,7 +4,10 @@ export default function RestrictedPage() {
   return (
     <div className={classes.restrictedContainer}>
       <h1>Access Denied</h1>
-      <div>You have reached a restricted route.</div>
+      <div>
+        You have reached a restricted route. If you are trying to log in with
+        Google, make sure you are whitelisted!
+      </div>
     </div>
   );
 }
